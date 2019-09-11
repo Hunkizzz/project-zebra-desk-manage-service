a46802e08efffc93b3ffe6a80a587be7b70bd150
hunk <hunkizzzx777@gmail.com>
2019-09-12 01:50:25 +0300

    HOTFIX workspace instead workplace in API/

--- src/main/java/team/projectzebra/util/exceptions/ValidationFailedException.java
+++ src/main/java/team/projectzebra/util/exceptions/ValidationFailedException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * Proudly created by dgonyak on 09/09/2019.
  */
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class ValidationFailedException extends Exception {
     private static final long serialVersionUID = 2L;
 
