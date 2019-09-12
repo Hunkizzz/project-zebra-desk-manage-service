3050d42f04387facc9e58b4fd179dd2e5b77887f
masdmtr <masdmtr@gmail.com>
2019-09-12 03:15:42 +0300

    add CORE for testing

--- src/main/java/team/projectzebra/controller/WorkspaceController.java
+++ src/main/java/team/projectzebra/controller/WorkspaceController.java
@@ -19,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +42,7 @@ import team.projectzebra.util.exceptions.ValidationFailedException;
 
 @Api(value = "Workspaces Booking Management System")
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/projectzebrateam-workspace-reservation-service")
 public class WorkspaceController {
     WorkspaceRepository workspaceRepository;
