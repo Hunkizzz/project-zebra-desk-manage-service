ef24bddf9178814438b929513d0026dfcd094b7c
hunk <hunkizzzx777@gmail.com>
2019-09-12 22:03:14 +0300

    Fix merge problems

--- src/main/java/team/projectzebra/persistence/repository/WorkspaceRepository.java
+++ src/main/java/team/projectzebra/persistence/repository/WorkspaceRepository.java
@@ -14,17 +14,17 @@ import java.util.UUID;
 public interface WorkspaceRepository extends JpaRepository<Workspace, UUID> {
     Workspace findByUuid(UUID uuid);
 
-    @Query(value = "SELECT 'Free' as state, count(busy) as count FROM workspace w where busy = false\n" +
+    @Query(value = "SELECT 'Free' as option, count(busy) as count FROM workspace w where busy = false\n" +
             "            UNION ALL\n" +
             "            \n" +
-            "            SELECT 'Busy' as state, count(busy) FROM workspace where busy = true\n" +
+            "            SELECT 'Busy' as option, count(busy) FROM workspace where busy = true\n" +
             "            UNION ALL\n" +
-            "            Select 'Total' as state, count(*) FROM workspace\n" +
+            "            Select 'Total' as option, count(*) FROM workspace\n" +
             "UNION ALL\n" +
-            "\tSELECT 'MGR' as state, count(*) FROM workspace w LEFT JOIN workspace_restriction  wr on w.uuid = wr.workspace_uuid where wr.type = 'MGR'\n" +
+            "\tSELECT 'MGR' as option, count(*) FROM workspace w LEFT JOIN workspace_restriction  wr on w.uuid = wr.workspace_uuid where wr.type = 'MGR'\n" +
             "\t\n" +
             "\tUNION ALL\n" +
-            "\tSELECT 'Equipped' as state, count(*) FROM workspace w where w.equipped = true;", nativeQuery = true)
+            "\tSELECT 'Equipped' as option, count(*) FROM workspace w where w.equipped = true;", nativeQuery = true)
     List<WorkspaceDto> getInfoAboutPlaces();
 
 //    @Query(value = "SELECT new team.projectzebra.dao.ReservationLogDao(w.uuid, wm.buildingCompany) from Workspace w left join Floor f on wm.uuid = workspace_meta_uuid where w.uuid = :uuid")
