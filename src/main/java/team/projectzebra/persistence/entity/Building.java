dd9ca43626e28e4a310c157bf29df9113150d570
dgonyak <dgonyak@deloitte.ru>
2019-09-19 13:32:37 +0300

    Changed API

--- src/main/java/team/projectzebra/persistence/entity/Building.java
+++ src/main/java/team/projectzebra/persistence/entity/Building.java
@@ -20,18 +20,22 @@ public class Building {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID uuid;
+    @ApiModelProperty(notes = "The company")
+    @OneToOne
+    @JoinColumn(name = "company")
+    private Company company;
     @ApiModelProperty(notes = "The building name")
     private String name;
     @ApiModelProperty(notes = "The building capacity")
     private Integer capacity;
+    @ApiModelProperty(notes = "The building zipcode")
+    private String zip;
+    @ApiModelProperty(notes = "The building state province")
+    private String stateProvince;
     @ApiModelProperty(notes = "The building city")
     private String city;
     @ApiModelProperty(notes = "The building street")
     private String street;
     @ApiModelProperty(notes = "The building number")
-    private String building;
-    @ApiModelProperty(notes = "The company")
-    @OneToOne
-    @JoinColumn(name = "company")
-    private Company company;
+    private String number;
 }
